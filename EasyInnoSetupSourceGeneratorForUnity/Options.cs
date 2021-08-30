5a0e521d876d84bca26f9898413a85564a7def6d
Kodai Takao (Fork HomeWorkstation) <kodai100@gmail.com>
2021-08-30 16:07:22 +0900

    initial commit

---
+++ EasyInnoSetupSourceGeneratorForUnity/Options.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CommandLine;
+
+namespace EasyInnoSetupForUnity
+{
+    class Options
+    {
+
+        [Value(0, MetaName = "Template File Path")]
+        public string TemplatePath { get; set; }
+
+        [Value(1, MetaName = "ISS Output Path")]
+        public string ISSOutputPath { get; set; }
+
+
+        [Option('i', "uuid", Required = true, HelpText = "App UUID")]
+        public string Uuid { get; set; }
+
+        [Option('n', "name", Required = true, HelpText = "App Name")]
+        public string AppName { get; set; }
+
+        [Option('v', "version", Required = false, HelpText = "App Version")]
+        public string AppVersion{ get; set; }
+
+        [Option('p', "publisher", Required = true, HelpText = "App Publisher")]
+        public string AppPublisher { get; set; }
+
+        [Option('u', "url", Required = false, HelpText = "App URL")]
+        public string AppUrl { get; set; }
+
+        [Option('e', "exename", Required = true, HelpText = "App Exe Name")]
+        public string AppExeName { get; set; }
+
+        [Option('r', "resource_dir", Required = true, HelpText = "Unity Artifact Resource Directory (the folder contains .exe)")]
+        public string ResourceDir { get; set; }
+
+        [Option('o', "iss_output_dir", Required = true, HelpText = "Installer Output Directory")]
+        public string OutputDir { get; set; }
+
+        [Option('b', "output_base_file_name", Required = true, HelpText = "Installer Output Base File Name")]
+        public string OutputBaseFileName { get; set; }
+    }
+}
---
+++ EasyInnoSetupSourceGeneratorForUnity/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CommandLine;
+
+namespace EasyInnoSetupForUnity
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed(HandleParsed)
+                   .WithNotParsed(HandleNotParsed);
+
+            
+        }
+
+        private static void HandleParsed(Options o)
+        {
+
+            // read
+            var text = File.ReadAllText(o.TemplatePath);
+
+
+
+            var replaced = text.Replace("##APP_UUID##", o.Uuid);
+            replaced = replaced.Replace("##APP_NAME##", o.AppName);
+
+            if(string.IsNullOrEmpty(o.AppVersion))
+            {
+                replaced = replaced.Replace("##APP_VERSION##", "");
+            }
+            else
+            {
+                replaced = replaced.Replace("##APP_VERSION##", o.AppVersion);
+            }
+            
+            replaced = replaced.Replace("##PUBLISHER##", o.AppPublisher);
+
+            if(string.IsNullOrEmpty(o.AppUrl))
+            {
+                replaced = replaced.Replace("##APP_URL##", "");
+            }
+            else
+            {
+                replaced = replaced.Replace("##APP_URL##", o.AppUrl);
+            }
+            
+            replaced = replaced.Replace("##APP_EXE_NAME##", o.AppExeName);
+            replaced = replaced.Replace("##RESOURCE_DIR##", o.ResourceDir);
+            replaced = replaced.Replace("##OUTPUT_DIR##", o.OutputDir);
+            replaced = replaced.Replace("##OUTPUT_BASE_FILE_NAME##", o.OutputBaseFileName);
+
+            // export
+            File.WriteAllText(o.ISSOutputPath, replaced);
+
+        }
+
+        private static void HandleNotParsed(IEnumerable<Error> errors)
+        {
+
+            Console.Error.WriteLine("Argument Error. Exit");
+
+            foreach(var e in errors)
+            {
+                Console.Error.WriteLine(e.ToString());
+            }
+
+            return;
+        }
+
+    }
+
+
+
+
+}
